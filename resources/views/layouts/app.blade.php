e2940e061336ce03e183225c841c6c70c1fe0982
makzoite <maheshraj.mainali@gmail.com>
2021-02-03 14:18:39 -0500

    Added Metronic theme and some implementation in our project

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
+
 <!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" {{ Metronic::printAttrs('html') }} {{ Metronic::printClasses('html') }}>
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -7,81 +8,63 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'FixitJA') }}</title>
+    {{-- Title Section --}}
+        <title>{{ config('app.name') }} | @yield('title', $page_title ?? '')</title>
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    {{-- Meta Data --}}
+    <meta name="description" content="@yield('page_description', $page_description ?? '')"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+
+    {{-- Favicon --}}
+    <link rel="shortcut icon" href="{{ asset('media/logos/favicon.ico') }}" />
+
+    {{-- Fonts --}}
+    {{ Metronic::getGoogleFontsInclude() }}
+
+    {{-- Global Theme Styles (used by all pages) --}}
+    @foreach(config('layout.resources.css') as $style)
+        <link href="{{ config('layout.self.rtl') ? asset(Metronic::rtlCssPath($style)) : asset($style) }}" rel="stylesheet" type="text/css"/>
+    @endforeach
+
+    {{-- Layout Themes (used by all pages) --}}
+    @foreach (Metronic::initThemes() as $theme)
+        <link href="{{ config('layout.self.rtl') ? asset(Metronic::rtlCssPath($theme)) : asset($theme) }}" rel="stylesheet" type="text/css"/>
+    @endforeach
+
+    {{-- Includable CSS --}}
+    @yield('styles')
+
     <script src="{{ asset('js/custom.js') }}" defer></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/custom-css.css') }}" rel="stylesheet">
-    @yield('modern-css')
+    <link href="{{ asset('css/modern-business.css') }}" rel="stylesheet">
 </head>
-<body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'FixitJA') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+<body {{ Metronic::printAttrs('body') }} {{ Metronic::printClasses('body') }}>
+    {{--@include('layouts.partials._navbar')
+    @yield('content'))--}}
+    @guest
+        @include('layouts.partials._navbar')
+        @yield('content')
+    @endguest
+    @auth
+        @if (config('layout.page-loader.type') != '')
+            @include('demo1_layout.partials._page-loader')
+        @endif
+        @include('layouts.base._layout')
+    @endauth
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
+    <script>var HOST_URL = "{{ route('quick-search') }}";</script>
 
-                    </ul>
+    {{-- Global Config (global config for global JS scripts) --}}
+    <script>
+        var KTAppSettings = {!! json_encode(config('layout.js'), JSON_PRETTY_PRINT|JSON_UNESCAPED_SLASHES) !!};
+    </script>
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
-                            
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
+    {{-- Global Theme JS Bundle (used by all pages)  --}}
+    @foreach(config('layout.resources.js') as $script)
+        <script src="{{ asset($script) }}" type="text/javascript"></script>
+    @endforeach
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
-        <main>
-            @yield('content')
-        </main>
-    </div>
+    {{-- Includable JS --}}
+    @yield('scripts')
 </body>
 </html>
