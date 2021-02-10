2520a9d44531852d5bb3a88d5d9add828772a6fe
makzoite <maheshraj.mainali@gmail.com>
2021-02-10 11:58:55 -0500

    simpe css changes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -71,7 +71,7 @@ $show_sidebar = false;
         <!--begin::Content-->
         <!--begin::Aside-->
         <div class="login-aside order-1 order-lg-2 bgi-no-repeat bgi-position-x-right">
-            <div class="login-conteiner bgi-no-repeat bgi-position-x-right bgi-position-y-bottom" style="background-size:400px; background-image: url({{asset('images/website/repair1.svg')}});">
+            <div class="login-conteiner bgi-no-repeat bgi-position-x-right bgi-position-y-bottom" style="background-image: url({{asset('images/website/repair1.svg')}});">
                 <!--begin::Aside title-->
                 <h3 class="pt-lg-40 pl-lg-20 pb-lg-0 pl-10 py-20 m-0 d-flex justify-content-lg-start font-weight-boldest display5 display1-lg text-white">We Got
                     <br />A Surprise
