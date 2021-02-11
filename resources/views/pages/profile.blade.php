402134dddf6802601abf1f107ca33460969e57fe
AchyutNeupane <achyutkneupane@gmail.com>
2021-02-12 02:37:53 +0545

    Profile and User added in Admin Panel

--- resources/views/pages/profile.blade.php
+++ resources/views/pages/profile.blade.php
@@ -1,5 +1,73 @@
+{{-- Author: Achyut Neupane --}}
+
 @extends('layouts.app')
 @section('content')
 
-AAA
+    <div class="row">
+
+        @include('admin.userSideBar', $user)
+
+        <div class="col-lg-8">
+            <div class="card card-custom">
+                <div class="card-header flex-wrap border-0 pt-6 pb-0">
+                    <div class="card-title">
+                        <h3 class="card-label">
+                            User Overview
+                        </h3>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <div class="form-group row">
+                        <label class="col-xl-3 col-lg-3 col-form-label">Address: </label>
+                        <div class="col-lg-9 col-xl-6">
+                            <span class="form-control form-control-lg form-control-solid">{{ $user->city->name }}</span>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-xl-3 col-lg-3 col-form-label">Experience: </label>
+                        <div class="col-lg-9 col-xl-6">
+                            <span class="form-control form-control-lg form-control-solid">{{ $user->experience }}</span>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-xl-3 col-lg-3 col-form-label">Website: </label>
+                        <div class="col-lg-9 col-xl-6">
+                            <span class="form-control form-control-lg form-control-solid"><a href="{{ $user->website }}">{{ $user->website }}</a></span>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-xl-3 col-lg-3 col-form-label">Is willing to travel to work?</label>
+                        <div class="col-lg-9 col-xl-6">
+                            <span class="form-control form-control-lg form-control-solid">
+                                @if($user->is_travelling == 1)
+                                Yes
+                                @else
+                                No
+                                @endif
+                            </span>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-xl-3 col-lg-3 col-form-label">Has Police report?</label>
+                        <div class="col-lg-9 col-xl-6">
+                            <span class="form-control form-control-lg form-control-solid">
+                                @if($user->is_police_record == 1)
+                                Yes
+                                @else
+                                No
+                                @endif
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+@endsection
+
+{{-- Scripts Section --}}
+@section('scripts')
+    <script src="{{ asset('js/pages/widgets.js') }}" type="text/javascript"></script>
 @endsection
