7f5bb2a58c06946d7e2c9336a99fc562e405fd05
makzoite <maheshraj.mainali@gmail.com>
2021-02-15 16:49:06 -0500

    design changes

--- resources/views/pages/createTaskWizard.blade.php
+++ resources/views/pages/createTaskWizard.blade.php
@@ -13,8 +13,8 @@
                             <!--begin::Wizard Step 1 Nav-->
                             <div class="wizard-step" data-wizard-type="step" data-wizard-state="current">
                                 <div class="wizard-label">
-                                    <i class="wizard-icon flaticon-bus-stop"></i>
-                                    <h3 class="wizard-title">1. Setup Location</h3>
+                                    <i class="wizard-icon flaticon-folder-1"></i>
+                                    <h3 class="wizard-title">1. Project Category</h3>
                                 </div>
                                 <span class="svg-icon svg-icon-xl wizard-arrow">
                                     <!--begin::Svg Icon | path:/metronic/theme/html/demo1/dist/assets/media/svg/icons/Navigation/Arrow-right.svg-->
@@ -32,8 +32,8 @@
                             <!--begin::Wizard Step 2 Nav-->
                             <div class="wizard-step" data-wizard-type="step" data-wizard-state="pending">
                                 <div class="wizard-label">
-                                    <i class="wizard-icon flaticon-list"></i>
-                                    <h3 class="wizard-title">2. Enter Details</h3>
+                                    <i class="wizard-icon flaticon-clipboard"></i>
+                                    <h3 class="wizard-title">2. Project Descriptions</h3>
                                 </div>
                                 <span class="svg-icon svg-icon-xl wizard-arrow">
                                     <!--begin::Svg Icon | path:/metronic/theme/html/demo1/dist/assets/media/svg/icons/Navigation/Arrow-right.svg-->
@@ -51,8 +51,8 @@
                             <!--begin::Wizard Step 3 Nav-->
                             <div class="wizard-step" data-wizard-type="step" data-wizard-state="pending">
                                 <div class="wizard-label">
-                                    <i class="wizard-icon flaticon-responsive"></i>
-                                    <h3 class="wizard-title">3. Select Services</h3>
+                                    <i class="wizard-icon flaticon-placeholder-1"></i>
+                                    <h3 class="wizard-title">3. User Information & Address</h3>
                                 </div>
                                 <span class="svg-icon svg-icon-xl wizard-arrow">
                                     <!--begin::Svg Icon | path:/metronic/theme/html/demo1/dist/assets/media/svg/icons/Navigation/Arrow-right.svg-->
@@ -70,41 +70,12 @@
                             <!--begin::Wizard Step 4 Nav-->
                             <div class="wizard-step" data-wizard-type="step" data-wizard-state="pending">
                                 <div class="wizard-label">
-                                    <i class="wizard-icon flaticon-truck"></i>
-                                    <h3 class="wizard-title">4. Delivery Address</h3>
+                                    <i class="wizard-icon flaticon-globe"></i>
+                                    <h3 class="wizard-title">4. Review & Submit</h3>
                                 </div>
-                                <span class="svg-icon svg-icon-xl wizard-arrow">
-                                    <!--begin::Svg Icon | path:/metronic/theme/html/demo1/dist/assets/media/svg/icons/Navigation/Arrow-right.svg-->
-                                    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
-                                        <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                                            <polygon points="0 0 24 0 24 24 0 24"></polygon>
-                                            <rect fill="#000000" opacity="0.3" transform="translate(12.000000, 12.000000) rotate(-90.000000) translate(-12.000000, -12.000000)" x="11" y="5" width="2" height="14" rx="1"></rect>
-                                            <path d="M9.70710318,15.7071045 C9.31657888,16.0976288 8.68341391,16.0976288 8.29288961,15.7071045 C7.90236532,15.3165802 7.90236532,14.6834152 8.29288961,14.2928909 L14.2928896,8.29289093 C14.6714686,7.914312 15.281055,7.90106637 15.675721,8.26284357 L21.675721,13.7628436 C22.08284,14.136036 22.1103429,14.7686034 21.7371505,15.1757223 C21.3639581,15.5828413 20.7313908,15.6103443 20.3242718,15.2371519 L15.0300721,10.3841355 L9.70710318,15.7071045 Z" fill="#000000" fill-rule="nonzero" transform="translate(14.999999, 11.999997) scale(1, -1) rotate(90.000000) translate(-14.999999, -11.999997)"></path>
-                                        </g>
-                                    </svg>
-                                    <!--end::Svg Icon-->
-                                </span>
                             </div>
                             <!--end::Wizard Step 4 Nav-->
-                            <!--begin::Wizard Step 5 Nav-->
-                            <div class="wizard-step" data-wizard-type="step" data-wizard-state="pending">
-                                <div class="wizard-label">
-                                    <i class="wizard-icon flaticon-globe"></i>
-                                    <h3 class="wizard-title">5. Review and Submit</h3>
-                                </div>
-                                <span class="svg-icon svg-icon-xl wizard-arrow last">
-                                    <!--begin::Svg Icon | path:/metronic/theme/html/demo1/dist/assets/media/svg/icons/Navigation/Arrow-right.svg-->
-                                    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
-                                        <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                                            <polygon points="0 0 24 0 24 24 0 24"></polygon>
-                                            <rect fill="#000000" opacity="0.3" transform="translate(12.000000, 12.000000) rotate(-90.000000) translate(-12.000000, -12.000000)" x="11" y="5" width="2" height="14" rx="1"></rect>
-                                            <path d="M9.70710318,15.7071045 C9.31657888,16.0976288 8.68341391,16.0976288 8.29288961,15.7071045 C7.90236532,15.3165802 7.90236532,14.6834152 8.29288961,14.2928909 L14.2928896,8.29289093 C14.6714686,7.914312 15.281055,7.90106637 15.675721,8.26284357 L21.675721,13.7628436 C22.08284,14.136036 22.1103429,14.7686034 21.7371505,15.1757223 C21.3639581,15.5828413 20.7313908,15.6103443 20.3242718,15.2371519 L15.0300721,10.3841355 L9.70710318,15.7071045 Z" fill="#000000" fill-rule="nonzero" transform="translate(14.999999, 11.999997) scale(1, -1) rotate(90.000000) translate(-14.999999, -11.999997)"></path>
-                                        </g>
-                                    </svg>
-                                    <!--end::Svg Icon-->
-                                </span>
-                            </div>
-                            <!--end::Wizard Step 5 Nav-->
+
                         </div>
                     </div>
                     <!--end::Wizard Nav-->
@@ -836,45 +807,7 @@
                                     </div>
                                 </div>
                                 <!--end::Wizard Step 4-->
-                                <!--begin::Wizard Step 5-->
-                                <div class="pb-5" data-wizard-type="step-content">
-                                    <!--begin::Section-->
-                                    <h4 class="mb-10 font-weight-bold text-dark">Review your Details and Submit</h4>
-                                    <h6 class="font-weight-bolder mb-3">Current Address:</h6>
-                                    <div class="text-dark-50 line-height-lg">
-                                        <div>Address Line 1</div>
-                                        <div>Address Line 2</div>
-                                        <div>Melbourne 3000, VIC, Australia</div>
-                                    </div>
-                                    <div class="separator separator-dashed my-5"></div>
-                                    <!--end::Section-->
-                                    <!--begin::Section-->
-                                    <h6 class="font-weight-bolder mb-3">Delivery Details:</h6>
-                                    <div class="text-dark-50 line-height-lg">
-                                        <div>Package: Complete Workstation (Monitor, Computer, Keyboard &amp; Mouse)</div>
-                                        <div>Weight: 25kg</div>
-                                        <div>Dimensions: 110cm (w) x 90cm (h) x 150cm (L)</div>
-                                    </div>
-                                    <div class="separator separator-dashed my-5"></div>
-                                    <!--end::Section-->
-                                    <!--begin::Section-->
-                                    <h6 class="font-weight-bolder mb-3">Delivery Service Type:</h6>
-                                    <div class="text-dark-50 line-height-lg">
-                                        <div>Overnight Delivery with Regular Packaging</div>
-                                        <div>Preferred Morning (8:00AM - 11:00AM) Delivery</div>
-                                    </div>
-                                    <div class="separator separator-dashed my-5"></div>
-                                    <!--end::Section-->
-                                    <!--begin::Section-->
-                                    <h6 class="font-weight-bolder mb-3">Delivery Address:</h6>
-                                    <div class="text-dark-50 line-height-lg">
-                                        <div>Address Line 1</div>
-                                        <div>Address Line 2</div>
-                                        <div>Preston 3072, VIC, Australia</div>
-                                    </div>
-                                    <!--end::Section-->
-                                </div>
-                                <!--end::Wizard Step 5-->
+
                                 <!--begin::Wizard Actions-->
                                 <div class="d-flex justify-content-between border-top mt-5 pt-10">
                                     <div class="mr-2">
