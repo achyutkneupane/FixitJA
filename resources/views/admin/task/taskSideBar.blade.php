91f987f9a0513bf3914c7b64de7925f67ac08961
AchyutNeupane <achyutkneupane@gmail.com>
2021-02-17 22:30:11 +0545

    User and Task sidebar made dynamic

--- resources/views/admin/task/taskSideBar.blade.php
+++ resources/views/admin/task/taskSideBar.blade.php
@@ -1,101 +1,55 @@
-<!-- <div class="col-lg-4">
-    <div class="card card-custom">
-        <div class="card-header flex-wrap border-0 pt-6 pb-0">
-            <div class="card-title">
-                <h3 class="card-label">
-                    Task Detail
-                    <div class="text-muted pt-2 font-size-sm">
-                        Task Id: {{ $task->id }}
-                    </div>
-                </h3>
-            </div>
-        </div>
-        <div class="card-body">
-            @if (isset($task->related_task_id))
-                <div class="d-flex align-items-center justify-content-between mb-2">
-                    <span class="font-weight-bold">Related Task:</span>
-                    <span class="text-muted">
-                        <a href="{{ route('viewTask', $task->related_task_id) }}">
-                            {{ $task->related_task_id }}
-                        </a>
-                    </span>
-                </div>
-            @endif
-            <div class="d-flex align-items-center justify-content-between mb-2">
-                <span class="font-weight-bold">Status:</span>
-                <span class="text-muted">{{ ucwords($task->status) }}</span>
-            </div>
-            <div class="d-flex align-items-center justify-content-between mb-2">
-                <span class="font-weight-bold">Type:</span>
-                <span class="text-muted">{{ ucwords($task->type) }}</span>
-            </div>
-            <div class="d-flex align-items-center justify-content-between mb-2">
-                <span class="font-weight-bold">Category:</span>
-                <span
-                    class="text-muted">{{ $task->sub_category->name }}({{ $task->sub_category->category->name }})</span>
-            </div>
-            <div class="d-flex align-items-center justify-content-between mb-2">
-                <span class="font-weight-bold">Created:</span>
-                <span class="text-muted">{{ $task->created_at->diffForHumans() }}</span>
-            </div>
-
-            <div class="navi navi-bold navi-hover navi-active navi-link-rounded mt-5">
-                <div class="navi-item mb-2">
-                    <a href="{{ route('viewTask', $task->id) }}" class="navi-link py-4">
-                        <span class="navi-text font-size-lg">Task Overview</span>
-                    </a>
-                </div>
-                <div class="navi-item mb-2">
-                    <a href="{{ route('taskAssignedBy', $task->id) }}" class="navi-link py-4">
-                        <span class="navi-text font-size-lg">Assigned By</span>
-                    </a>
-                </div>
-                <div class="navi-item mb-2">
-                    <a href="{{ route('taskAssignedTo', $task->id) }}" class="navi-link py-4">
-                        <span class="navi-text font-size-lg">Assigned To</span>
-                    </a>
-                </div>
-            </div>
-        </div>
-    </div>
-</div> -->
+{{-- Author: Achyut Neupane --}}
 <div class="flex-row-auto offcanvas-mobile w-300px w-xl-350px" id="kt_profile_aside">
     <!--begin::Card-->
     <div class="card card-custom">
         <!--begin::Body-->
         <div class="card-body pt-15">
             <!--begin::User-->
-            <div class="text-center mb-10">
-                <div class="symbol symbol-60 symbol-circle symbol-xl-90">
-                    <div class="symbol-label" style="background-image:url('/metronic/theme/html/demo1/dist/assets/media/users/300_21.jpg')"></div>
-                    <i class="symbol-badge symbol-badge-bottom bg-success"></i>
+            <div class="mb-10">
+                <h4 class="font-weight-bold my-2">Task Detail</h4>
+                <div class="text-muted mb-6">Task Id: {{ $task->id }}</div>
+                @if (isset($task->related_task_id))
+                    <div class="d-flex align-items-center justify-content-between mb-2">
+                        <span class="font-weight-bold">Related Task:</span>
+                        <span class="text-muted">
+                            <a href="{{ route('viewTask', $task->related_task_id) }}">
+                                {{ $task->related_task_id }}
+                            </a>
+                        </span>
+                    </div>
+                @endif
+                <div class="d-flex align-items-center justify-content-between mb-2">
+                    <span class="font-weight-bold">Status:</span>
+                    <span class="text-muted">{{ ucwords($task->status) }}</span>
                 </div>
-                <h4 class="font-weight-bold my-2">James Jones</h4>
-                <div class="text-muted mb-2">Application Developer</div>
-                <span class="label label-light-warning label-inline font-weight-bold label-lg">Active</span>
+                <div class="d-flex align-items-center justify-content-between mb-2">
+                    <span class="font-weight-bold">Type:</span>
+                    <span class="text-muted">{{ ucwords($task->type) }}</span>
+                </div>
+                <div class="d-flex align-items-center justify-content-between mb-2">
+                    <span class="font-weight-bold">Category:</span>
+                    <span
+                        class="text-muted">{{ $task->sub_category->name }}({{ $task->sub_category->category->name }})</span>
+                </div>
+                <div class="d-flex align-items-center justify-content-between mb-2">
+                    <span class="font-weight-bold">Created:</span>
+                    <span class="text-muted">{{ $task->created_at->diffForHumans() }}</span>
+                </div>
             </div>
             <!--end::User-->
-            <!--begin::Contact-->
-            <div class="mb-10 text-center">
-                <a href="#" class="btn btn-icon btn-circle btn-light-facebook mr-2">
-                    <i class="socicon-facebook"></i>
-                </a>
-                <a href="#" class="btn btn-icon btn-circle btn-light-twitter mr-2">
-                    <i class="socicon-twitter"></i>
-                </a>
-                <a href="#" class="btn btn-icon btn-circle btn-light-google">
-                    <i class="socicon-google"></i>
-                </a>
-            </div>
-            <!--end::Contact-->
             <!--begin::Nav-->
-            <a href="#" class="btn btn-hover-light-primary font-weight-bold py-3 px-6 mb-2 text-center btn-block active">Profile Overview</a>
-            <a href="#" class="btn btn-hover-light-primary font-weight-bold py-3 px-6 mb-2 text-center btn-block">Personal info</a>
-            <a href="#" class="btn btn-hover-light-primary font-weight-bold py-3 px-6 mb-2 text-center btn-block">Account Info</a>
-            <a href="#" class="btn btn-hover-light-primary font-weight-bold py-3 px-6 mb-2 text-center btn-block">Change Passwort</a>
-            <a href="#" class="btn btn-hover-light-primary font-weight-bold py-3 px-6 mb-2 text-center btn-block">Email Settings</a>
-            <a href="#" class="btn btn-hover-light-primary font-weight-bold py-3 px-6 mb-2 text-center btn-block">Saved Credit Cards</a>
-            <a href="#" class="btn btn-hover-light-primary font-weight-bold py-3 px-6 mb-2 text-center btn-block">Tax information</a>
+            <a href="{{ route('viewTask', $task->id) }}"
+                class="btn btn-hover-light-primary font-weight-bold py-3 px-6 mb-2 btn-block active">
+                Task Overview
+            </a>
+            <a href="{{ route('taskAssignedBy', $task->id) }}"
+                class="btn btn-hover-light-primary font-weight-bold py-3 px-6 mb-2 btn-block active">
+                Assigned By
+            </a>
+            <a href="{{ route('taskAssignedTo', $task->id) }}"
+                class="btn btn-hover-light-primary font-weight-bold py-3 px-6 mb-2 btn-block active">
+                Assigned To
+            </a>
             <!--end::Nav-->
         </div>
         <!--end::Body-->
--- resources/views/admin/userSideBar.blade.php
+++ resources/views/admin/userSideBar.blade.php
@@ -1,44 +1,46 @@
-<div class="col-lg-4">
+{{-- Author: Achyut Neupane --}}
+<div class="flex-row-auto offcanvas-mobile w-300px w-xl-350px" id="kt_profile_aside">
+    <!--begin::Card-->
     <div class="card card-custom">
-        <div class="card-header flex-wrap border-0 pt-6 pb-0">
-            <div class="card-title">
-                <h3 class="card-label">
-                    {{ ucwords($user->name) }}
-                    <div class="text-muted pt-2 font-size-sm">
+        <!--begin::Body-->
+        <div class="card-body pt-15">
+            <!--begin::User-->
+            <div class="mb-10">
+                <div class="row">
+                    <div class="col-12">
+                        <h4 class="float-left font-weight-bold my-2">{{ ucwords($user->name) }}</h4>
+                        <div class="float-right">
+                            <img src="
+                        @if (!is_null($user->documents->where('type',
+                            'profile_picture')->first())) {{ asset('storage/' . $user->documents->where('type', 'profile_picture')->first()->path) }}
+                        @else
+                            {{ asset('images/unknown-avatar.png') }} @endif
+                            " class="rounded-circle object-fit-scale-down" width="100" height="100">
+                        </div>
                     </div>
-                </h3>
-            </div>
-            <div class="card-toolbar">
-                <img src="
-                    @if(!is_null($user->documents->where('type', 'profile_picture')->first()))
-                    {{ asset('storage/'. $user->documents->where('type', 'profile_picture')->first()->path) }}
-                    @else
-                    {{ asset('images/unknown-avatar.png') }}
-                    @endif
-                " class="rounded-circle object-fit-scale-down" width="100" height="100">
-            </div>
-        </div>
-        <div class="card-body">
-            <div class="d-flex align-items-center justify-content-between mb-2">
-                <span class="font-weight-bold">Email:</span>
-                <span class="text-muted">{{ $user->email }}</span>
-            </div>
-            <div class="d-flex align-items-center justify-content-between mb-2">
-                <span class="font-weight-bold">Phone:</span>
-                <span class="text-muted">{{ $user->phone }}</span>
-            </div>
-            <div class="d-flex align-items-center justify-content-between mb-2">
-                <span class="font-weight-bold">Gender:</span>
-                <span class="text-muted">{{ $user->gender }}</span>
-            </div>
-
-            <div class="navi navi-bold navi-hover navi-active navi-link-rounded mt-5">
-                <div class="navi-item mb-2">
-                    <a href="{{ route('viewUser', $user->id) }}" class="navi-link py-4">
-                        <span class="navi-text font-size-lg">User Overview</span>
-                    </a>
+                </div>
+                <div class="d-flex align-items-center justify-content-between mb-2">
+                    <span class="font-weight-bold">Email:</span>
+                    <span class="text-muted">{{ $user->email }}</span>
+                </div>
+                <div class="d-flex align-items-center justify-content-between mb-2">
+                    <span class="font-weight-bold">Phone:</span>
+                    <span class="text-muted">{{ $user->phone }}</span>
+                </div>
+                <div class="d-flex align-items-center justify-content-between mb-2">
+                    <span class="font-weight-bold">Gender:</span>
+                    <span class="text-muted">{{ ucwords($user->gender) }}</span>
                 </div>
             </div>
+            <!--end::User-->
+            <!--begin::Nav-->
+            <a href="{{ Auth::user()->id === $user->id ? route('viewProfile') : route('viewUser', $user->id) }}"
+                class="btn btn-hover-light-primary font-weight-bold py-3 px-6 mb-2 btn-block active">
+                User Overview
+            </a>
+            <!--end::Nav-->
         </div>
+        <!--end::Body-->
     </div>
+    <!--end::Card-->
 </div>
