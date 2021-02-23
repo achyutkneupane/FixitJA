e1745244eb9eff0793040b60f4b1a3ecb537697a
makzoite <maheshraj.mainali@gmail.com>
2021-02-23 14:32:03 -0500

    working on showing status of email verification and application status

--- resources/views/layouts/base/_aside.blade.php
+++ resources/views/layouts/base/_aside.blade.php
@@ -1,13 +1,13 @@
 {{-- Aside --}}
 
 @php
-    $kt_logo_image = 'logo-light.png';
+$kt_logo_image = 'logo-light.png';
 @endphp
 
 @if (config('layout.brand.self.theme') === 'light')
-    @php $kt_logo_image = 'logo-dark.png' @endphp
+@php $kt_logo_image = 'logo-dark.png' @endphp
 @elseif (config('layout.brand.self.theme') === 'dark')
-    @php $kt_logo_image = 'logo-light.png' @endphp
+@php $kt_logo_image = 'logo-light.png' @endphp
 @endif
 
 <div class="aside aside-left {{ Metronic::printClasses('aside', false) }} d-flex flex-column flex-row-auto" id="kt_aside">
@@ -16,14 +16,14 @@
     <div class="brand flex-column-auto {{ Metronic::printClasses('brand', false) }}" id="kt_brand">
         <div class="brand-logo">
             <a href="{{ url('/') }}">
-                <img alt="{{ config('app.name') }}" height="60px" src="{{ asset('images/logo.png') }}"/>
+                <img alt="{{ config('app.name') }}" height="60px" src="{{ asset('images/logo.png') }}" />
             </a>
         </div>
 
         @if (config('layout.aside.self.minimize.toggle'))
-            <button class="brand-toggle btn btn-sm px-0" id="kt_aside_toggle">
-                {{ Metronic::getSVG("media/svg/icons/Navigation/Angle-double-left.svg", "svg-icon-xl") }}
-            </button>
+        <button class="brand-toggle btn btn-sm px-0" id="kt_aside_toggle">
+            {{ Metronic::getSVG("media/svg/icons/Navigation/Angle-double-left.svg", "svg-icon-xl") }}
+        </button>
         @endif
 
     </div>
@@ -32,17 +32,13 @@
     <div class="aside-menu-wrapper flex-column-fluid" id="kt_aside_menu_wrapper">
 
         @if (config('layout.aside.self.display') === false)
-            <div class="header-logo">
-                <a href="{{ url('/') }}">
-                    <img alt="{{ config('app.name') }}" height="60px" src="{{ asset('images/logo.png') }}"/>
-                </a>
-            </div>
+        <div class="header-logo">
+            <a href="{{ url('/') }}">
+                <img alt="{{ config('app.name') }}" height="60px" src="{{ asset('images/logo.png') }}" />
+            </a>
+        </div>
         @endif
-        <div
-            id="kt_aside_menu"
-            class="aside-menu my-4 {{ Metronic::printClasses('aside_menu', false) }}"
-            data-menu-vertical="1"
-            {{ Metronic::printAttrs('aside_menu') }}>
+        <div id="kt_aside_menu" class="aside-menu my-4 {{ Metronic::printClasses('aside_menu', false) }}" data-menu-vertical="1" {{ Metronic::printAttrs('aside_menu') }}>
 
             <ul class="menu-nav {{ Metronic::printClasses('aside_menu_nav', false) }}">
                 {{ Menu::renderVerMenu(config('menu_aside.items')) }}
--- resources/views/layouts/base/_content.blade.php
+++ resources/views/layouts/base/_content.blade.php
@@ -2,6 +2,8 @@
 @if (config('layout.content.extended'))
     @yield('content')
 @else
+    @include('layouts.partials._custom_alert_heading', ['email_not_verified' => 'true'])
+    @include('layouts.partials._custom_alert_heading', ['applicaiton_not_complete' => 'true'])
     <div class="d-flex flex-column-fluid">
         <div class="{{ Metronic::printClasses('content-container', false) }}">
             @yield('content')
